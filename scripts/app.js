f18781d031111a05b9b788c9fdcf697552cac1aa
Adachima <sapondanaisriwan@gmail.com>
2022-11-19 22:52:30 +0700

    .

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,44 @@
-const playerConfigOverlayElement = document.getElementById("config-overlay");
-const backdropElement = document.getElementById('backdrop');
-const formElement = document.querySelector('form');
-const errorsOutputElement =  document.getElementById('config-errors');
+let editedPlayer = 0;
+let activePlayer = 0;
 
-const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
-const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
-const cancelConfigBtnElement = document.getElementById('cancel-config-btn')
+const players = [
+  {
+    name: "",
+    symbol: "X",
+  },
+  {
+    name: "",
+    symbol: "O",
+  },
+];
+
+const playerConfigOverlayElement = document.getElementById("config-overlay");
+const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
+// const gameFieldElements = document.querySelectorAll('#game-board li')
+const gameBoardElement = document.getElementById("game-board");
+const activePlayerNameElement = document.getElementById("active-player-name");
+
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
-cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
-backdropElement.addEventListener('click', closePlayerConfig);
+cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
 
-formElement.addEventListener('submit', savePlayerConfig);
+formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
+
+// loop through every li element
+// for(const gameFieldElement of gameFieldElements) {
+//     gameFieldElement.addEventListener('click', selectGameField)
+// }
+
+gameBoardElement.addEventListener("click", selectGameField);
--- scripts/config.js
+++ scripts/config.js
@@ -1,33 +1,44 @@
-function openPlayerConfig() {
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid; // + แปลง string ไปเป็น int(ตัวเลข) +'1' => 1
+  console.log(editedPlayer);
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
 }
 
 function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
-    formElement.firstElementChild.classList.remove('error')
-    errorsOutputElement.textContent = '';
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = '';
 }
 
 function savePlayerConfig(event) {
-    // Clicking on a "Submit" button, prevent it from submitting a form 
-    // Clicking on a link, prevent the link from following the URL
-    // สรุป ป้องกันไม่ให้กดส่งแบบ form ก่อนหรือกด url ก่อน
-    event.preventDefault(); // preventing browser sending a request automaitclly
-    console.log(event);
-    const formData = new FormData(event.target);
-    // '    Max God   ' => 'Max God'
-    const enteredPlayername = formData.get('playername').trim();
-    console.log(enteredPlayername)
+  // Clicking on a "Submit" button, prevent it from submitting a form
+  // Clicking on a link, prevent the link from following the URL
+  // สรุป ป้องกันไม่ให้กดส่งแบบ form ก่อนหรือกด url ก่อน
+  event.preventDefault(); // preventing browser sending a request automaitclly
+  console.log(event);
+  const formData = new FormData(event.target);
+  // '    Max God   ' => 'Max God'
+  const enteredPlayername = formData.get("playername").trim();
+  console.log(enteredPlayername);
 
-    // alternative way eneteredplayername === ""
-    if (!enteredPlayername) { // check if eneterplayername is empty string "" 
-        event.target.firstElementChild.classList.add('error');
-        errorsOutputElement.textContent = 'Please eneter a valid name';
-        return;
-    }
+  // alternative way eneteredplayername === ""
+  if (!enteredPlayername) {
+    // check if eneterplayername is empty string ""
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please eneter a valid name";
+    return;
+  }
 
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  console.log(updatedPlayerDataElement.children[1].textContent);
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
 
-    
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
 }
